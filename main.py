21a9df59a89357831827836248d47e5bdfef9e33
Bharath NV <39437901+Bharathwitu@users.noreply.github.com>
2021-04-06 20:16:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ MDTextField :
     
 """
 headers = {
-        "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36'
+        "User-Agent": ' '       # Copy And Paste your User-Agent 
 }
 
 
